8061461bd6281fa05c0cedf4a333dba912c15484
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-06 12:59:37 +0000

    store a function in an object

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ function startGame() {
   alert('Game is starting ...');
 }
 
+const person = {
+  greet: function greet() {
+    console.log('Hello there!!');
+  }
+};
+person.greet();
 startGameBtn.addEventListener('click', startGame);
