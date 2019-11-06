6a86273e2f3500fa5ced315ce29c74153fc94d9a
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-06 18:22:54 +0000

    add win function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -25,7 +28,21 @@ const getComputerChoice = function() {
   } else if (randomValue < 0.67) {
     return PAPER;
   } else {
-    return;
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice == pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
   }
 };
 
@@ -36,5 +53,10 @@ startGameBtn.addEventListener('click', function() {
   gameIsRunning = true;
   console.log('Game is starting ...');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+  console.log('playerSelection: ' + playerSelection);
+  console.log('computerChoice:  ' + computerChoice);
+  console.log(winner);
+  console.log('Game ended!');
 });
