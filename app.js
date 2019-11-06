e63e66807e03f55d3f1dc42c398b3d41bcb95afe
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-06 17:24:00 +0000

    prevent starting the game when its running

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
 const getPlayerChoice = function() {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
@@ -16,6 +19,10 @@ const getPlayerChoice = function() {
 };
 
 startGameBtn.addEventListener('click', function() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting ...');
   const playerSelection = getPlayerChoice();
   console.log(playerSelection);
