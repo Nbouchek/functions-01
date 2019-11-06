7c3db9534bb7659e578fc4c93bc994ca77c2ae10
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-06 17:14:12 +0000

    INTRODUCE GLOBAL CONSTANTS.

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const startGameBtn = document.getElementById('start-game-btn');
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
-
+const DEFAULT_USER_CHOICE = ROCK;
 const getPlayerChoice = function() {
-  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
-  if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ) {
-    alert('Invalid choice, we chose Rock for you!');
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice, we chose ${DEFAULT_USER_CHOICE} for you!`);
   }
 };
 
